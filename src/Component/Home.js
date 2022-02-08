13c42b895a315452a688d3b59d36dce4cb0fa404
rainbowyoutheens <rainbowyoutheens@gmail.com>
2022-02-08 16:31:24 +0100

    first commit

---
+++ src/Component/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from "./Subcomponents/Navbar";
+import "../App.css";
+
+function Home() {
+  return <div>
+    <Navbar />
+    <section className="home">
+      <h1>Hello World</h1>      
+      <button></button>
+      <div></div>      
+    </section>
+    <section>
+      <h1>Welcome to Rainbow Youtheens</h1>
+    </section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+  </div>;
+}
+
+export default Home;
---
+++ src/Component/Subcomponents/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+  return <header>
+      <nav className="nav-bar">
+          <img src="#"></img>
+          <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Academy</li>
+              <li>Blog</li>
+              <li>Events</li>
+              <li>Contact</li>
+          </ul>
+      </nav>
+  </header>;
+}
+
+export default Navbar;
