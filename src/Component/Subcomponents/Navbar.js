be00aafdc1d15df2958ad1b3680374ccfbbeb10e
Rainbowyoutheens <98896874+Rainbowyoutheens@users.noreply.github.com>
2022-02-11 18:31:46 +0100

    Update Navbar.js

--- src/Component/Subcomponents/Navbar.js
+++ src/Component/Subcomponents/Navbar.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
-  return <header>
-      <nav className="nav navbar">
-          <img src="#"></img>
-          <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Academy</li>
-              <li>Blog</li>
-              <li>Events</li>
-              <li>Contact</li>
-          </ul>
+    return <div>
+      <nav id="navBar" class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+        <div className="container-fluid">
+            <Link to="/" class="navbar-brand">
+                <img src="/myImages/rytlogo2.png" alt="logo" width="120" height="40"/>
+            </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <Link to="/" class="nav-link active" aria-current="page">HOME</Link>
+                    </li>                    
+                    <li class="nav-item">
+                        <Link to="/About" class="nav-link active">ABOUT US</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/Academy" class="nav-link active">ACADEMY</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/Blog" class="nav-link active">BLOG</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/Events" class="nav-link active">EVENTS</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/Contact" class="nav-link active">CONTACT</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
       </nav>
-  </header>;
-}
+    </div>;
+  }
 
 export default Navbar;
